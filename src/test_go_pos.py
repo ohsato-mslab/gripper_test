0177119061baf04aab3c33538b83436dc9051f60
ohsato-mslab <ohsato@ms.esys.tsukuba.ac.jp>
2020-01-17 03:23:51 +0000

    go_pos changed

--- src/test_go_pos.py
+++ src/test_go_pos.py
@@ -6,13 +6,13 @@ import rospy
 
 #初期設定
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)#17-INLOCK
-GPIO.setup(27, GPIO.OUT)#27-START
-GPIO.setup(19, GPIO.OUT)#19-POS4
-GPIO.setup(22, GPIO.OUT)#22-POS3
-GPIO.setup(13, GPIO.OUT)#13-POS2
-GPIO.setup(10, GPIO.OUT)#10-POS1
-GPIO.setup(11, GPIO.OUT)#11-POS0
+GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)#17-INLOCK
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)#27-START
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)#19-POS4
+GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)#22-POS3
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)#13-POS2
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)#10-POS1
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)#11-POS0
 
 rospy.init_node('gripper', anonymous=True)
 
@@ -21,59 +21,59 @@ try:
     while True:
         GPIO.output(17,GPIO.HIGH)
         print "INLOCK is unlocked. 5 seconds sleep.."
-        rospy.sleep(5.)
+        rospy.sleep(0.1)
         #ここから位置決め　0番
         GPIO.output(19,GPIO.LOW)
         GPIO.output(22,GPIO.LOW)
         GPIO.output(13,GPIO.LOW)
         GPIO.output(10,GPIO.LOW)#すべてのPOSビットを0に
-        GPIO.output(11,GPIO.LOW)
+        GPIO.output(11,GPIO.HIGH)
         now = rospy.Time.now()
         print(now)
-        rospy.sleep(0.1)#t1待つ
+        rospy.sleep(0.015)#t1待つ
         now = rospy.Time.now()
         print(now)
         GPIO.output(27,GPIO.HIGH)#実行命令
         print "send Position0. 3 seconds sleep.."
-        rospy.sleep(3)
+        rospy.sleep(0.1)
         GPIO.output(27,GPIO.LOW)
-        rospy.sleep(3)
+        rospy.sleep(4)
         #ここから位置決め　10番
         GPIO.output(19,GPIO.LOW)
         GPIO.output(22,GPIO.HIGH)
         GPIO.output(13,GPIO.LOW)
         GPIO.output(10,GPIO.HIGH)
         GPIO.output(11,GPIO.LOW)
-        rospy.sleep(0.1)#t1待つ
+        rospy.sleep(0.015)#t1待つ
         GPIO.output(27,GPIO.HIGH)#実行命令
         print "send Position10. 3 seconds sleep.."
-        rospy.sleep(3)
+        rospy.sleep(0.1)
         GPIO.output(27,GPIO.LOW)
-        rospy.sleep(3)
+        rospy.sleep(0.5)
         #ここから位置決め　20番
         GPIO.output(19,GPIO.HIGH)
         GPIO.output(22,GPIO.LOW)
         GPIO.output(13,GPIO.HIGH)
         GPIO.output(10,GPIO.LOW)
         GPIO.output(11,GPIO.LOW)#すべてのPOSビットを0に
-        rospy.sleep(0.1)#t1待つ
+        rospy.sleep(0.015)#t1待つ
         GPIO.output(27,GPIO.HIGH)#実行命令
         print "send Position20. 3 seconds sleep.."
-        rospy.sleep(3)
+        rospy.sleep(0.1)
         GPIO.output(27,GPIO.LOW)
-        rospy.sleep(3)
+        rospy.sleep(0.5)
         #ここから位置決め　31番
         GPIO.output(19,GPIO.HIGH)
         GPIO.output(22,GPIO.HIGH)
         GPIO.output(13,GPIO.HIGH)
         GPIO.output(10,GPIO.HIGH)
         GPIO.output(11,GPIO.HIGH)#すべてのPOSビットを0に
-        rospy.sleep(0.1)#t1待つ
+        rospy.sleep(0.015)#t1待つ
         GPIO.output(27,GPIO.HIGH)#実行命令
         print "send Position32. 10 seconds sleep.."
         rospy.sleep(3)
         GPIO.output(27,GPIO.LOW)
-        rospy.sleep(10)
+        rospy.sleep(2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
